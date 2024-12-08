c9110d3966b92e456e32e0773cc3411c121ad641
Patrick Glissmann <patrick.glissmann@hotmail.de>
2024-12-08 21:22:16 +0100

    changed submenu to dialog element

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function load() {
   renderSidebar();
+  initSubmenu();
 }
 
 function renderSidebar() {
--- scripts/submenu.js
+++ scripts/submenu.js
@@ -1,4 +1,34 @@
-function toggleSubmenu() {
-  document.getElementById("submenu").classList.toggle("hidden_submenu");
-  document.activeElement?.blur();
+function initSubmenu() {
+  let submenu = document.getElementById("submenu_dialog");
+  addOutsideClickClosingListener(submenu);
+  addEscapeListener(submenu);
+}
+
+function openSubmenu() {
+  let submenu = document.getElementById("submenu_dialog");
+  submenu.showModal();
+}
+
+function addOutsideClickClosingListener(element) {
+  element.addEventListener("click", (e) => {
+    const elementDimensions = element.getBoundingClientRect();
+    if (
+      e.clientX < elementDimensions.left ||
+      e.clientX > elementDimensions.right ||
+      e.clientY < elementDimensions.top ||
+      e.clientY > elementDimensions.bottom
+    ) {
+      element.close();
+      document.getElementById("header_user_profile").blur();
+    }
+  });
+}
+
+function addEscapeListener(element) {
+  element.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      element.close();
+      document.getElementById("header_user_profile").blur();
+    }
+  });
 }
