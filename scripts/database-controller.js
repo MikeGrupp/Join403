d77479643bce65a2aabf387904201d61b90096bc
Patrick Glissmann <patrick.glissmann@hotmail.de>
2024-12-10 20:09:31 +0100

    changed merged db-field for name

--- scripts/database-controller.js
+++ scripts/database-controller.js
@@ -33,62 +33,52 @@ async function postData(path = "", data = {}) {
 
 function initContactsTable() {
   postData("/contacts", {
-    "last-name": "Müller",
-    "first-name": "Sofia",
+    name: "Sofia Müller",
     mail: "sofia.mueller@example.com",
     phone: "015123456789",
   });
   postData("/contacts", {
-    "last-name": "Meier",
-    "first-name": "Emma",
+    name: "Emma Meier",
     mail: "emma.meier@test.de",
     phone: "017654321098",
   });
   postData("/contacts", {
-    "last-name": "Mertens",
-    "first-name": "Lucas",
+    name: "Lucas Mertens",
     mail: "lucas.mertens@webmail.com",
     phone: "01521987654",
   });
   postData("/contacts", {
-    "last-name": "Schmidt",
-    "first-name": "Hannah",
+    name: "Hannah Schmidt",
     mail: "hannah.schmidt@gmail.com",
     phone: "01623456789",
   });
   postData("/contacts", {
-    "last-name": "Weber",
-    "first-name": "Jonas",
+    name: "Jonas Weber",
     mail: "jonas.weber@outlook.de",
     phone: "01755432198",
   });
   postData("/contacts", {
-    "last-name": "Fischer",
-    "first-name": "Anna",
+    name: "Anna Fischer",
     mail: "anna.fischer@gmx.net",
     phone: "01876543210",
   });
   postData("/contacts", {
-    "last-name": "Wagner",
-    "first-name": "Maximilian",
+    name: "Maximilian Wagner",
     mail: "maximilian.wagner@hotmail.com",
     phone: "01987654321",
   });
   postData("/contacts", {
-    "last-name": "Klein",
-    "first-name": "Sophie",
+    name: "Sophie Klein",
     mail: "sophie.klein@web.de",
     phone: "016234567890",
   });
   postData("/contacts", {
-    "last-name": "Hoffmann",
-    "first-name": "Felix",
+    name: "Felix Hoffmann",
     mail: "felix.hoffmann@email.com",
     phone: "01711234567",
   });
   postData("/contacts", {
-    "last-name": "Koch",
-    "first-name": "Maria",
+    name: "Maria Koch",
     mail: "maria.koch@t-online.de",
     phone: "01907654321",
   });
