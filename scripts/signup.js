2d433cc039be24c8e73f03cefdda73e4540e3e55
Mike Grupp <mike.grupp68@gmail.com>
2024-12-16 01:34:42 +0100

    login and signup css

--- scripts/signup.js
+++ scripts/signup.js
@@ -8,3 +8,11 @@ function addUser() {
     users.push({email: email.value, password: password.value});
     window.location.href = 'login.html?msg=Du hast dich erfolgreich registriert';
 }
+
+function previousPage() {
+    if (window.history.length > 1) {
+        window.history.back();
+    } else {
+        window.location.href = 'index.html';
+    }
+}
